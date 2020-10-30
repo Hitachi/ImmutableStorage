8feaa8332c3a8b2df8e57a46059c2f71022a0866
eiichiro-oiwa <eiichiro.oiwa.nm@hitachi.com>
2020-10-30 16:44:55 +0900

    containerization

--- immutil/config.go
+++ immutil/config.go
@@ -8,58 +8,82 @@ import (
 )
 
 const (
-	TmplDir = "/var/lib/ImmutableDS/tmpl"
-	ConfBaseDir = "/var/lib/ImmutableDS/org"
+	TmplDir = "/var/lib/ImmutableST/tmpl"
+	VolBaseDir = "/var/lib/ImmutableST/org"
 	CAHostname = "ca"
 	HttpdHostname = "www"
 	ImmsrvHostname = "immsrv"
 	EnvoyHostname = "envoy"
 	TlsCAHostname = "tlsca"
+	DinDHostname = "dind"
 
 	CaImg = "hyperledger/fabric-ca:1.4.6"
 	OrdererImg = "hyperledger/fabric-orderer:1.4.6"
 	CouchDBImg = "hyperledger/fabric-couchdb:amd64-0.4.20"
 	PeerImg = "hyperledger/fabric-peer:1.4.6"
-	DockerImg = "docker:19.03.9-dind"
+	DockerImg = "library/docker:19.03.9-dind"
 	
-	ImmHttpdImg = "httpd:2.4.46"
+	ImmHttpdImg = "library/httpd:2.4.46"
 
-	ImmSrvImg = "ubuntu:20.10"
+	ImmSrvImg = "library/ubuntu:20.10"
 	EnvoyImg = "envoyproxy/envoy:v1.14.1"
+
+	ChainCcenvImg = "hyperledger/fabric-ccenv:1.4.7"
+	ChainBaseOsImg = "hyperledger/fabric-baseos:amd64-0.4.18"
 
 	configYaml = "config.yaml"
 
 	K8sSubDomain = "imm"
 	K8sLocalSvc = ".default.svc.cluster.local"
-	
+
 	DockerNetPrefix = "net_hfl_"
+
+	defaultCertCountry = `["JP"]`
+	defaultCertLocality = `["Tokyo"]`
+	defaultCertProvince = `["Shinagawa"]`
+
+	workVolume = "work-vol"
 )
 
 type ImmConfig struct {
 	Subj pkix.Name
 	NetName string `yaml:"DockerNetname"`
 	ExternalIPs []string `yaml:"ExternalIPs"`
+	Registry string `yaml:"Registry"`
+	RegistryAuth string `yaml:"RegsitryAuth"`
 }
 
-func ReadConf(org string) (config *ImmConfig, retErr error) {
-	config = &ImmConfig{}
-	
-	confFile := ConfBaseDir + "/" + org + "/" + configYaml
+func ReadConfigWithDefaultFile(org string) (config *ImmConfig, retErr error) {
+	return ReadConfigWithFile(org, VolBaseDir + "/" + org + "/" + configYaml)
+}
+
+func ReadConfigWithFile(org, confFile string) (config *ImmConfig, retErr error) {
 	confBuf, err := ioutil.ReadFile(confFile)
 	if err != nil {
 		retErr = fmt.Errorf("could not read " + confFile + ": " + err.Error())
 		return
 	}
 
-	err = yaml.Unmarshal(confBuf, config)
+	config, retErr = convertYamlToStruct(org, confBuf)
+	if retErr != nil {
+		return
+	}
+
+	retErr = k8sWriteOrgConfig(org, string(confBuf), nil)
+	return
+}
+
+func convertYamlToStruct(org string, src []byte) (config *ImmConfig, retErr error) {
+	config = &ImmConfig{}
+	err := yaml.Unmarshal(src, config)
 	if err != nil {
 		retErr = fmt.Errorf("could not read network configuration")
 		return
 	}
 	
-	err = yaml.Unmarshal(confBuf, &config.Subj)
+	err = yaml.Unmarshal(src, &config.Subj)
 	if err != nil {
-		retErr = fmt.Errorf("could not read a subject")
+		retErr = fmt.Errorf("could not read a subject for certificate")
 		return
 	}
 
@@ -69,5 +93,15 @@ func ReadConf(org string) (config *ImmConfig, retErr error) {
 		config.NetName = DockerNetPrefix+org
 	}
 
+	return
+}
+
+func ReadOrgConfig(org string) (config *ImmConfig, retOrg string, retErr error) {
+	config, retErr = k8sGenerateOrgConfig(org)
+	if retErr != nil {
+		return
+	}
+
+	retOrg = config.Subj.Organization[0]
 	return
 }
